9dc68bdd2c28c215cd9156699b480f1cffc0c93a
onlee1425 <enflenffl@gmail.com>
2023-03-23 15:58:32 +0900

    feat : kakao 로그인/로그아웃/토큰갱신 기능 구현

--- src/main/java/com/ecloth/beta/member/service/MemberService.java
+++ src/main/java/com/ecloth/beta/member/service/MemberService.java
@@ -1,18 +1,19 @@
 package com.ecloth.beta.member.service;
 
+import com.ecloth.beta.common.jwt.JwtTokenProvider;
+import com.ecloth.beta.common.jwt.JwtTokenUtil;
 import com.ecloth.beta.member.component.JavaMailSenderComponent;
+import com.ecloth.beta.member.dto.InfoMeRequest;
 import com.ecloth.beta.member.dto.MemberRequest;
 import com.ecloth.beta.member.dto.Token;
 import com.ecloth.beta.member.entity.Member;
 import com.ecloth.beta.member.exception.ErrorCode;
-import com.ecloth.beta.member.exception.GlobalCustomException;
-import com.ecloth.beta.common.jwt.JwtTokenProvider;
-import com.ecloth.beta.common.jwt.JwtTokenUtil;
+import com.ecloth.beta.member.exception.MemberException;
 import com.ecloth.beta.member.model.MemberRole;
 import com.ecloth.beta.member.model.MemberStatus;
 import com.ecloth.beta.member.repository.MemberRepository;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpHeaders;
@@ -23,14 +24,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
 @Transactional
-@AllArgsConstructor
 @Builder
+@RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -38,17 +40,18 @@ public class MemberService {
     private final JavaMailSenderComponent javaMailSenderComponent;
     private final JwtTokenProvider jwtTokenProvider;
     private final JwtTokenUtil jwtTokenUtil;
-    private final RedisTemplate<String,String> redisTemplate;
+    private final RedisTemplate<String, String> redisTemplate;
+    private final OauthService oauthService;
 
     @Transactional
     public Member register(MemberRequest.Register RegisterDto) {
         // 이메일 중복 체크
         if (memberRepository.existsByEmail(RegisterDto.getEmail())) {
-            throw new GlobalCustomException(ErrorCode.ALREADY_EXIST_EMAIL);
+            throw new MemberException(ErrorCode.ALREADY_EXIST_EMAIL);
         }
         // 닉네임 중복 체크
         if (memberRepository.existsByNickname(RegisterDto.getNickname())) {
-            throw new GlobalCustomException(ErrorCode.ALREADY_EXIST_NICKNAME);
+            throw new MemberException(ErrorCode.ALREADY_EXIST_NICKNAME);
         }
         // 이메일 인증 코드 생성
         String emailAuthCode = UUID.randomUUID().toString().replace("-", "");
@@ -78,7 +81,7 @@ public class MemberService {
     // 이메일 인증 완료 후, 멤버 정보를 업데이트하는 메소드
     public void updateMemberAfterEmailAuth(String emailAuthCode) {
         Member member = memberRepository.findByEmailAuthCode(emailAuthCode)
-                .orElseThrow(() -> new GlobalCustomException(ErrorCode.INVALID_EMAIL_AUTH_CODE));
+                .orElseThrow(() -> new MemberException(ErrorCode.INVALID_EMAIL_AUTH_CODE));
 
         // 이메일 인증 완료 후, 멤버 정보 업데이트
         member = member.toBuilder()
@@ -94,10 +97,10 @@ public class MemberService {
     public HttpHeaders login(MemberRequest.Login loginDto) {
         // 이메일 검증
         Member member = memberRepository.findByEmail(loginDto.getEmail())
-                .orElseThrow(() -> new GlobalCustomException(ErrorCode.NOT_FOUND_USER));
+                .orElseThrow(() -> new MemberException(ErrorCode.NOT_FOUND_USER));
         // 비밀번호 검증
         if (!passwordEncoder.matches(loginDto.getPassword(), member.getPassword())) {
-            throw new GlobalCustomException(ErrorCode.WRONG_PASSWORD);
+            throw new MemberException(ErrorCode.WRONG_PASSWORD);
         }
         // AccessToken, Refresh Token 생성하기
         Token token = jwtTokenProvider.generateToken(member.getEmail());
@@ -107,8 +110,8 @@ public class MemberService {
 
         // AccessToken과 RefreshToken Http Header에 담아 반환하기
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + token.getAccessToken());
-        headers.add("RefreshToken", "Bearer " + token.getRefreshToken());
+        headers.add("authorization", "Bearer " + token.getAccessToken());
+        headers.add("refreshtoken", "Bearer " + token.getRefreshToken());
         return headers;
     }
 
@@ -116,47 +119,118 @@ public class MemberService {
     public HttpHeaders reissueToken(String requestRT) {
         // "Bearer " 제거
         requestRT = requestRT.replace("Bearer ", "");
-        // request RefreshToken 검증
-        if (!jwtTokenProvider.validateToken(requestRT)) {
-            throw new GlobalCustomException(ErrorCode.EXPIRED_OR_INVALID_REFRESH_TOKEN);
-        }
-        // request RefreshToken 에서 이메일 가져오기
-        String email = jwtTokenProvider.getEmail(requestRT);
-        // Redis 에서 email 을 키값으로 저장된 RT 가져오기
-        String redisRT = (String) redisTemplate.opsForValue().get("RT:" + email);
-        // request RefreshToken과 Redis의 RefreshToken이 일치하지 않는 경우
-        if (!requestRT.equals(redisRT)) {
-            throw new GlobalCustomException(ErrorCode.EXPIRED_OR_INVALID_REFRESH_TOKEN);
-        }
-
-        // 새로운 토큰 생성
-        Token token = jwtTokenProvider.generateToken(email);
-        // RefreshToken Redis에 업데이트
-        redisTemplate.opsForValue()
-                .set("RT:" + email, token.getRefreshToken()
-                        , token.getRefreshTokenExpirationTime(), TimeUnit.MILLISECONDS);
+        // SecurityContext 에서 현재 인증 객체 가져오기
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // User email,role  가져오기
+        String email = authentication.getName();
+        String role = authentication.getAuthorities().toString();
+        log.info("토큰갱신요청 사용자 이메일 : " + email);
+        log.info("토큰갱신요청 사용자 역할 : " + role);
 
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + token.getAccessToken());
-        headers.add("RefreshToken", "Bearer " + token.getRefreshToken());
-        return headers;
+
+        // 이메일 회원일경우
+        if (role.equals("[[ROLE_MEMBER]]")) {
+            log.info("ROLE_MEMBER 확인 토큰갱신 진행");
+            // 새로운 토큰 생성
+            Token token = jwtTokenProvider.generateToken(email);
+            // RefreshToken Redis 에 업데이트
+            redisTemplate.opsForValue()
+                    .set("RT:" + email, token.getRefreshToken()
+                            , token.getRefreshTokenExpirationTime(), TimeUnit.MILLISECONDS);
+
+            headers.add("authorization", "Bearer " + token.getAccessToken());
+            headers.add("refreshtoken", "Bearer " + token.getRefreshToken());
+            log.info("토큰갱신 AT : " + token.getAccessToken());
+            log.info("토큰갱신 RT : " + token.getRefreshToken());
+            return headers;
+
+            // 카카오 회원일경우
+        } else if (role.equals("[[ROLE_OAUTH_MEMBER]]")) {
+            log.info("ROLE_OAUTH_MEMBER 확인 토큰갱신 진행");
+
+            String redisKRT = redisTemplate.opsForValue().get("KRT:" + email);
+
+            // Redis에서 저장된 kakao RefreshToken 값의 유효시간을 일 단위로 확인
+            Long ttl = redisTemplate.getExpire("KRT:" + email, TimeUnit.DAYS);
+            log.info("Redis key KRT:{} has TTL {} days", email, ttl);
+
+            if (ttl != null && ttl < 30) {
+                // 카카오 RefreshToken 유효기간이 30일 미만일경우 reissueKakaoToken 실행
+                oauthService.reissueKakaoToken(redisKRT, email);
+            }
+
+            // 새로운 JWT토큰 생성
+            Token token = jwtTokenProvider.generateToken(email);
+            // RefreshToken Redis 에 업데이트
+            redisTemplate.opsForValue()
+                    .set("RT:" + email, token.getRefreshToken()
+                            , token.getRefreshTokenExpirationTime(), TimeUnit.MILLISECONDS);
+
+            headers.add("authorization", "Bearer " + token.getAccessToken());
+            headers.add("refreshtoken", "Bearer " + token.getRefreshToken());
+
+            return headers;
+
+        } else {
+            throw new RuntimeException("유효하지 않은 회원 유형입니다.");
+        }
     }
 
     public void logout(String accessToken) {
         // "Bearer " 제거
         accessToken = accessToken.replace("Bearer ", "");
-        // SecurityContext에서 현재 인증 객체 가져오기
+        // SecurityContext 에서 현재 인증 객체 가져오기
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        // User email을 가져오기
+        // User email 을 가져오기
         String email = authentication.getName();
+        String role = authentication.getAuthorities().toString();
 
-        // Redis에서 해당 User email로 저장된 Refresh Token 확인 후 있을 경우 삭제
-        if (redisTemplate.opsForValue().get("RT:" + email) != null) {
-            // 해당 Access Token 유효시간을 가지고 와서 BlackList에 저장
-            Long expiration = jwtTokenProvider.getExpiration(accessToken);
-            jwtTokenUtil.setBlackListToken(email, accessToken, expiration);
-            jwtTokenUtil.deleteRefreshToken(email);
+        // 이메일 회원일경우
+        if (role.equals("[[ROLE_MEMBER]]")) {
+            log.info("ROLE_MEMBER 확인 로그아웃 진행");
+
+            // Redis 에서 해당 User email 로 저장된 Refresh Token 확인 후 있을 경우 삭제
+            if (redisTemplate.opsForValue().get("RT:" + email) != null) {
+                // 해당 Access Token 유효시간을 가지고 와서 BlackList 에 저장
+                Long expiration = jwtTokenProvider.getExpiration(accessToken);
+                jwtTokenUtil.setBlackListToken(email, accessToken, expiration);
+                jwtTokenUtil.deleteRefreshToken(email);
+            }
+        } else if (role.equals("[[ROLE_OAUTH_MEMBER]]")) {
+            log.info("ROLE_OAUTH_MEMBER 확인 로그아웃 진행");
+
+            if (redisTemplate.opsForValue().get("KRT:" + email) != null &&
+                    redisTemplate.opsForValue().get("RT:" + email) != null) {
+
+                // 해당 JWT Access Token 유효시간을 가지고 와서 BlackList 에 저장
+                Long expiration = jwtTokenProvider.getExpiration(accessToken);
+                jwtTokenUtil.setBlackListToken(email, accessToken, expiration);
+                jwtTokenUtil.deleteRefreshToken(email);
+
+                // KRT 삭제
+                jwtTokenUtil.deleteKakaoRefreshToken(email);
+
+            }
         }
+    }
+
+    // 회원 정보 조회
+    public InfoMeRequest getInfoMe() {
+        // SecurityContext 에서 현재 인증 객체 가져오기
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // User email  가져오기
+        String email = authentication.getName();
+        Optional<Member> member = memberRepository.findByEmail(email);
+        if (member.isEmpty()) {
+            throw new MemberException(ErrorCode.NOT_FOUND_USER);
+        }
+        return InfoMeRequest.builder()
+                .email(member.get().getEmail())
+                .nickname(member.get().getNickname())
+                .phone(member.get().getPhone())
+                .profileImagePath(member.get().getProfileImagePath())
+                .build();
     }
 
 }
