e4dc3cd2bf3ec1a5b4c777d6f81c855290e9d702
simdev1234 <simdev1234@gmail.com>
2023-04-04 23:58:56 +0900

    fix : 채팅 기능 수정
    메세지 응답 형식 수정 - 등록일 기준 오름차순

--- src/main/java/com/ecloth/beta/domain/chat/service/ChatMessageService.java
+++ src/main/java/com/ecloth/beta/domain/chat/service/ChatMessageService.java
@@ -3,13 +3,12 @@ package com.ecloth.beta.domain.chat.service;
 import com.ecloth.beta.domain.chat.document.ChatMessage;
 import com.ecloth.beta.domain.chat.dto.ChatMessageSendRequest;
 import com.ecloth.beta.domain.chat.dto.ChatMessageListResponse;
-import com.ecloth.beta.domain.chat.dto.ChatMessageListResponse.ChatMessageResponse;
 import com.ecloth.beta.domain.chat.repository.ChatMessageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -25,7 +24,9 @@ public class ChatMessageService {
 
         chatMessageRepository.save(request.toEntity());
 
-        return ChatMessageListResponse.fromEntity(chatMessageRepository.findAll());
+        List<ChatMessage> chatMessages = chatMessageRepository.findAllByChatRoomIdOrderByRegisterDate(request.getChatRoomId());
+
+        return ChatMessageListResponse.fromEntity(chatMessages);
     }
 
 }
