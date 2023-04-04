77db4f3ebc02ba83641aa68cfb946ffeb751a068
simdev1234 <simdev1234@gmail.com>
2023-04-04 22:53:37 +0900

    fix : 채팅 기능 수정
    StompConfig 설정 수정,
    Stomp Controller 예외 인터셉터 추가

--- src/main/java/com/ecloth/beta/config/WebSocketConfig.java
+++ src/main/java/com/ecloth/beta/config/WebSocketConfig.java
@@ -1,12 +1,15 @@
 package com.ecloth.beta.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
@@ -19,7 +22,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-connection")
-                .setAllowedOriginPatterns("*");
+                .setAllowedOriginPatterns("*")
+                .addInterceptors(new HttpSessionHandshakeInterceptor());
     }
 
 }
