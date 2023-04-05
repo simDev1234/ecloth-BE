06df2cffc91dd4b9874fc13c7433f4293589fc8d
onlee1425 <enflenffl@gmail.com>
2023-04-06 00:17:08 +0900

    Revert "fix : test 주석처리"
    This reverts commit f96e42e9530c13fdedb5c6c4d87a7c23da767aaa.

--- src/test/java/com/ecloth/beta/BetaApplicationTests.java
+++ src/test/java/com/ecloth/beta/BetaApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.ecloth.beta;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class BetaApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.ecloth.beta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
